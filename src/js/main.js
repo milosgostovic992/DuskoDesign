d870623500480f196d974b6c889d03cb9483b3bb
milosgostovic92 <milos.gostovic@vegait.rs>
2023-03-16 14:43:50 +0100

    Initial push

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import '../styles/style.scss';
+import documentLoaded from './chunks/documentLoaded';
+
+documentLoaded.init();
---
+++ vite.config.js
@@ -0,0 +1,56 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import fs from 'fs-extra';
+import babel from 'rollup-plugin-babel';
+import stylelint from 'vite-plugin-stylelint';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  root: './src',
+  logLevel: 'info',
+  plugins: [
+    stylelint({
+      failOnError: true,
+      cache: false,
+    }),
+    eslint({
+      include: ['src/**/*.js'],
+      exclude: ['node_modules/**', './*.config.js'],
+      fix: false,
+    }),
+  ],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name].js',
+        chunkFileNames: 'js/[name].js',
+        assetFileNames: 'styles/[name].[ext]',
+        manualChunks(id) {
+          if (id.endsWith('index.html')) {
+            fs.copySync('src/assets', './dist/assets');
+          }
+        },
+      },
+      plugins: [
+        babel({
+          exclude: ['node_modules/**'],
+          presets: ['@babel/preset-env'],
+        }),
+        stylelint({
+          failOnError: true,
+          cache: false,
+        }),
+        eslint({
+          include: ['dist/**/*.js'],
+          exclude: ['node_modules/**', './*.config.js'],
+          fix: false,
+        }),
+      ],
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
