2f23068e5b8807d1742b53791085907de7557a69
Srdjan <pajicsr@gmail.com>
2023-03-24 14:06:22 +0100

    Products section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import '../styles/style.scss';
 import documentLoaded from './chunks/documentLoaded';
 import header from './chunks/header';
+import swiper from './chunks/swiper';
+import products from './chunks/products';
 
 documentLoaded.init();
 header.init();
+swiper.init();
+products.init();
