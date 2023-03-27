1a0fa40ca49671554ff683331f233a8fdd3fabc6
Srdjan <pajicsr@gmail.com>
2023-03-27 13:25:31 +0200

    Add light mode

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,10 @@ import documentLoaded from './chunks/documentLoaded';
 import header from './chunks/header';
 import swiper from './chunks/swiper';
 import products from './chunks/products';
+import light from './chunks/light-mode';
 
 documentLoaded.init();
 header.init();
 swiper.init();
 products.init();
+light.init();
